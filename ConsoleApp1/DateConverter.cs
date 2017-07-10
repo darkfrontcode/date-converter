bbaa36543a9c873ff6a081dd1e4637a09683927e
Ueslei de Souza <darkfrontcode@gmail.com>
2017-07-10 18:41:39 -0300

    base

---
+++ ConsoleApp1/DateConverter.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateConverter
+{
+	class DateConverter
+	{
+		long day, month, year, hour, minute;
+		int[] calendar = new int[]{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+		public delegate void Operation(long value);
+		Operation Apply;
+
+		public string ChangeDate(String date, char op, long value)
+		{
+			ValidOperator(op);
+			Unmount(date);
+			value = Absolute(value);
+			ConfigOperation(op);
+			Apply(value);
+			return Mount();
+		}
+
+		#region actions
+
+		public void ValidOperator(char op)
+		{
+			if (op != '-' && op != '+') throw new Exception("Available operators: + | -");
+		}
+
+		public void Unmount(string date)
+		{
+			day		= int.Parse(date.Substring(0, 2));
+			month	= int.Parse(date.Substring(3, 2));
+			year	= int.Parse(date.Substring(6, 4));
+			hour	= int.Parse(date.Substring(11, 2));
+			minute	= int.Parse(date.Substring(14, 2));
+		}
+
+		public void ConfigOperation(char op)
+		{
+			Apply = op == '+' ? new Operation((long value) => Increase(value)) : new Operation((long value) => Decrease(value));
+		}
+
+		public void Increase(long value)
+		{
+			for (int i = 1; i <= value; i++) sumMinute();
+		}
+
+		public void Decrease(long value)
+		{
+			for (int i = 1; i <= value; i++) subMinute();
+		}
+
+		public long Absolute(long value)
+		{
+			return ( value < 0 ) ? Math.Abs(value) : value;
+		}
+
+		public string Mount()
+		{
+			return day.ToString("00") + "/" + month.ToString("00") + "/" + year + " " + hour.ToString("00") + ":" + minute.ToString("00");
+		}
+
+		#endregion
+
+		#region Operation SUM
+
+		public void sumYear()
+		{
+			++year;
+		}
+
+		public void sumMonth()
+		{
+			if (month == 12)
+			{
+				month = 1;
+				sumYear();
+			}
+			else
+			{
+				++month;
+			}
+		}
+
+		public void sumDay()
+		{
+			if (calendar[month] == day)
+			{
+				day = 1;
+				sumMonth();
+			}
+			else
+			{
+				++day;
+			}
+		}
+
+		public void sumHour()
+		{
+			if (hour == 23)
+			{
+				hour = 0;
+				sumDay();
+			}
+			else
+			{
+				++hour;
+			}
+		}
+
+		public void sumMinute()
+		{
+			if (minute == 59)
+			{
+				minute = 00;
+				sumHour();
+			}
+			else
+			{
+				++minute;
+			}
+		}
+		#endregion
+
+		#region Operation SUB
+		public void subYear()
+		{
+			--year;
+		}
+
+		public void subMonth()
+		{
+			if (month == 1)
+			{
+				month = 12;
+				subYear();
+			}
+			else
+			{
+				--month;
+			}
+		}
+
+		public void subDay()
+		{
+			if (day == 1)
+			{
+				day = calendar[month];
+				subMonth();
+			}
+			else
+			{
+				--day;
+			}
+		}
+
+		public void subHour()
+		{
+			if (hour == 0)
+			{
+				hour = 23;
+				subDay();
+			}
+			else
+			{
+				--hour;
+			}
+		}
+
+		public void subMinute()
+		{
+			if (minute == 00)
+			{
+				minute = 59;
+				subHour();
+			} 
+			else
+			{
+				--minute;
+			}
+		}
+
+		#endregion
+
+	}
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateConverter
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string date1 = new DateConverter().ChangeDate("01/03/2010 23:00", '+', 4000);
+			string date2 = new DateConverter().ChangeDate("01/03/2010 00:40", '-', 70);
+			string date3 = new DateConverter().ChangeDate("01/03/2010 23:00", '-', 30);
+			string date4 = new DateConverter().ChangeDate("26/02/2010 23:00", '+', 4020);
+			string date5 = new DateConverter().ChangeDate("26/02/2010 23:00", '+', 4020);
+			string date6 = new DateConverter().ChangeDate("01/03/2010 01:20", '-', 100);
+
+			Console.WriteLine(date1);
+			Console.WriteLine(date2);
+			Console.WriteLine(date3);
+			Console.WriteLine(date4);
+			Console.WriteLine(date5);
+			Console.WriteLine(date6);
+		}
+	}
+}
